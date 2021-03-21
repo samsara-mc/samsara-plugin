105de1a40bdd3c464094d7aa8fbbec4fe86eb476
Mia <maria.riezebos@gmail.com>
2021-03-21 16:36:34 +0100

    prepare project for moda module

---
+++ src/cx/mia/samsara/SamsaraModule.java
@@ -0,0 +1,17 @@
+package cx.mia.samsara;
+
+import cx.moda.moda.module.Module;
+import cx.mia.samsara.storage.SamsaraStorageHandler;
+
+public class SamsaraModule extends Module<SamsaraStorageHandler> {
+
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void onEnable() {
+    }
+}
---
+++ src/cx/mia/samsara/storage/SamsaraStorageHandler.java
@@ -0,0 +1,9 @@
+package cx.mia.samsara.storage;
+
+import cx.moda.moda.module.storage.ModuleStorageHandler;
+
+public interface SamsaraStorageHandler extends ModuleStorageHandler {
+
+
+
+}
