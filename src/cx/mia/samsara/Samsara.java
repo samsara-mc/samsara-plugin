7365bc4d165c586b6d559487094906336916ad19
Mia <maria.riezebos@gmail.com>
2021-03-21 21:38:59 +0100

    main module

---
+++ src/cx/mia/samsara/Samsara.java
@@ -0,0 +1,20 @@
+package cx.mia.samsara;
+
+import cx.moda.moda.module.Module;
+import cx.mia.samsara.storage.SamsaraStorageHandler;
+
+public class Samsara extends Module<SamsaraStorageHandler> {
+
+
+
+    @Override
+    public String getName() {
+        return "Samsara";
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
