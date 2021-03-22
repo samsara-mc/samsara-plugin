e5522c695e86d47a482c0a19686f9de3e1a7e723
Mia <maria.riezebos@gmail.com>
2021-03-22 05:55:20 +0100

    numerous bug fixes
    music now no longer double triggered nor phantom triggered.
    onRoomLeave and onRoomEnter methods that fire when respective events are called
    fixed the debug
    playSounds() and stopSounds() methods
    
    sounds now stored in Sound rather than String
    
    private api method to set a new roomLeaveListener
    
    api methods to add and removePlayers from the Room

--- src/cx/mia/samsara/api/Room.java
+++ src/cx/mia/samsara/api/Room.java
@@ -1,9 +1,7 @@
 package cx.mia.samsara.api;
 
 import cx.mia.samsara.Samsara;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.SoundCategory;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,68 +11,120 @@ import xyz.derkades.derkutils.bukkit.LocationUtils;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public abstract class Room implements Listener {
-
-    private final Samsara module;
+public class Room implements Listener {
 
     private final String name;
-    private final RoomLeaveListener roomLeaveListener;
-    private Location[] corners;
-    private ArrayList<String> sounds;
-    private ArrayList<Player> players;
-
     private RoomEnterListener roomEnterListener;
+    private RoomLeaveListener roomLeaveListener;
+    private Location[] corners;
+    private final ArrayList<Sound> sounds;
+    private final ArrayList<Player> players;
 
     /**
      * A Room is a defined space within a minecraft world that will play sounds while the player resides within them.
-     * @param name          name of the room
-     * @param c1       first corner of the room (cuboid)
-     * @param c2       second corner of the room (cuboid)
-     * @param sounds        a list of sounds sound associated with this room (these will play when entering the room, see {@link cx.mia.samsara.api.RoomEnterListener})
+     *
+     * @param name   name of the room
+     * @param c1     first corner of the room (cuboid)
+     * @param c2     second corner of the room (cuboid)
+     * @param sounds a list of sounds sound associated with this room (these will play when entering the room, see {@link cx.mia.samsara.api.RoomEnterListener})
      */
-    public Room(Samsara module, String name, Location c1, Location c2, String... sounds) {
+    public Room(String name, Location c1, Location c2, Sound... sounds) {
 
         // declare all local variables for the instance
-        this.module = module;
 
         this.name = name;
         this.corners = sortCorners(c1, c2);
         this.sounds = new ArrayList<>(Arrays.asList(sounds));
 
+        this.players = new ArrayList<>();
+
         this.roomEnterListener = new RoomEnterListener(this);
         this.roomLeaveListener = new RoomLeaveListener(this);
 
         // register the Listeners
-        module.registerListener(roomEnterListener);
+        getModule().registerListener(roomEnterListener);
+        getModule().registerListener(roomLeaveListener);
 
-        module.getLogger().debug("Declared room with name: ", getName());
-        module.getLogger().debug("Room ", getName(), " has corners: ", getCorners());
-        module.getLogger().debug("Room ", getName(), " has sounds: ", getSounds());
-        module.getLogger().debug("Room ", getName(), " has listener: ", getRoomEnterListener());
+        getModule().getLogger().debug("Declared room with name: " + getName());
+        getModule().getLogger().debug("Room " + getName() + " has corners: " + getCorners()[0].getX() + " " + getCorners()[0].getY() + " " + getCorners()[0].getZ() + " and " + getCorners()[1].getX() + " " + getCorners()[1].getY() + " " + getCorners()[1].getZ());
+        getModule().getLogger().debug("Room " + getName() + " has sounds: " + getSounds());
+        getModule().getLogger().debug("Room " + getName() + " has listener: " + getRoomEnterListener().toString());
 
     }
 
-//    @EventHandler(priority = EventPriority.LOWEST)
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onRoomEnter(RoomEnterEvent event) {
         Player player = event.getPlayer();
 
-        this.players.add(player);
+        if (event.getRoom().equals(this)) {
+            playSounds(player);
+            getModule().getLogger().debug(player.getName() + " entered " + event.getRoom().getName() + ", so all its sounds were triggered");
+            addPlayer(player);
+        } else {
+            if (getPlayers().contains(player)) {
+                stopSounds(player);
+                getModule().getLogger().debug(player.getName() + " entered a room other than " + this.getName() + ", so all its sounds were stopped");
+            }
+        }
 
-        sounds.forEach(s -> {
-            player.playSound(player.getLocation(), s, SoundCategory.VOICE, 1000f, 1f);
+//        if (!event.getRoom().equals(this) && getPlayers().contains(player)) {
+//            stopSounds(player);
+//            getModule().getLogger().debug(player.getName() + " entered a room other than " + this.getName() + ", so all its sounds were stopped");
+//            return;
+//        } else {
+//            playSounds(player);
+//            getModule().getLogger().debug(player.getName() + " entered " + event.getRoom().getName() + ", so all its sounds were triggered");
+//            addPlayer(player);
+//        }
+
+    }
+
+    @EventHandler
+    public void onRoomLeave(RoomLeaveEvent event) {
+        Player player = event.getPlayer();
+
+        if (event.getRoom().equals(this)) {
+            stopSounds(player);
+            getModule().getLogger().debug(player.getName() + " left " + event.getRoom().getName() + ", so all its sounds were stopped");
+            removePlayer(player);
+        } else {
+            if (getPlayers().contains(player)) {
+                playSounds(player);
+                getModule().getLogger().debug(player.getName() + " left a room other than " + this.getName() + ", but remains in this room, so all its sounds were triggered");
+            }
+        }
+
+//        if (!event.getRoom().equals(this) && getPlayers().contains(player)) {
+//            playSounds(player);
+//            getModule().getLogger().debug(player.getName() + " left a room other than " + this.getName() + ", but remains in this room, so all its sounds were triggered");
+//            return;
+//        } else {
+//            stopSounds(player);
+//            getModule().getLogger().debug(player.getName() + " left " + event.getRoom().getName() + ", so all its sounds were stopped");
+//            removePlayer(player);
+//        }
+    }
+
+    public void playSounds(Player player) {
+        sounds.forEach(sound -> {
+            sound.playSound(player);
+            getModule().getLogger().debug("Sound " + sound.getSound() + " was triggered by " + player.getName());
         });
     }
 
-//    @EventHandler
-    public void onRoomLeave(RoomLeaveEvent event) {
+    public void stopSounds(Player player) {
+        sounds.forEach(sound -> {
+            sound.stopSound(player);
+            getModule().getLogger().debug("Stopping sound " + sound.getSound() + " for player " + player.getName());
+        });
+    }
 
-
-        Player player = event.getPlayer();
-        sounds.forEach(player::stopSound);
+    public boolean playerInsideRoom(Player player) {
+        return LocationUtils.isIn3dBounds(player.getLocation(), corners[0], corners[1]);
     }
 
     private Samsara getModule() {
-        return this.module;
+        return Samsara.getInstance();
     }
 
     public String getName() {
@@ -89,7 +139,7 @@ public abstract class Room implements Listener {
         this.corners = sortCorners(c1, c2);
     }
 
-    public ArrayList<String> getSounds() {
+    public ArrayList<Sound> getSounds() {
         return this.sounds;
     }
 
@@ -106,18 +156,35 @@ public abstract class Room implements Listener {
 
     }
 
+    public RoomLeaveListener getRoomLeaveListener() {
+        return this.roomLeaveListener;
+    }
+
+    private void setRoomLeaveListener(RoomLeaveListener roomLeaveListener) {
+
+        RoomLeaveEvent.getHandlerList().unregister(getRoomLeaveListener());
+
+        this.roomLeaveListener = roomLeaveListener;
+        getModule().registerListener(getRoomLeaveListener());
+
+    }
+
     public ArrayList<Player> getPlayers() {
         return this.players;
     }
 
-    public boolean playerInsideRoom(Player player) {
-        return LocationUtils.isIn3dBounds(player.getLocation(), corners[0], corners[1]);
+    public void addPlayer(Player player) {
+        getPlayers().add(player);
+    }
+
+    private void removePlayer(Player player) {
+        getPlayers().remove(player);
     }
 
     public static Location[] sortCorners(Location corner1, Location corner2) {
 
-        return new Location[]{  LocationUtils.minCorner(corner1, corner2),
-                                LocationUtils.maxCorner(corner1, corner2)};
+        return new Location[]{LocationUtils.minCorner(corner1, corner2),
+                LocationUtils.maxCorner(corner1, corner2)};
 
     }
 }
