8163f1a8df051d46fa6ac8886b72cb5ff198e5ae
Mia <maria.riezebos@gmail.com>
2021-03-22 10:40:48 +0100

    all rooms

--- src/cx/mia/samsara/Samsara.java
+++ src/cx/mia/samsara/Samsara.java
@@ -4,6 +4,7 @@ import cx.mia.samsara.api.JoinQuitListener;
 import cx.mia.samsara.api.Room;
 import cx.mia.samsara.api.Sound;
 import cx.mia.samsara.commands.Teleport;
+import cx.mia.samsara.listeners.DamageListener;
 import cx.moda.moda.module.Module;
 import cx.mia.samsara.storage.SamsaraStorageHandler;
 import cx.moda.moda.module.command.ModuleCommandBuilder;
@@ -48,6 +49,107 @@ public class Samsara extends Module<SamsaraStorageHandler> {
      * @param world the world to look for locations of the rooms in.
      */
     private void registerRooms(World world) {
+
+
+
+        registerListener(
+                new Room(
+                        "start room",
+                        new Location(world, 270, 172, 84),
+                        new Location(world, 176, 221, -12),
+                        Sound.GENESIS_INCEPTION2
+                )
+        );
+
+        registerListener(
+                new Room(
+                        "life main",
+                        new Location(world, 166, 94, 203),
+                        new Location(world, 214, 64, 173),
+                        Sound.LIFE
+                )
+        );
+
+        registerListener(
+                new Room(
+                        "mirror room",
+                        new Location(world, 263, 186, 82),
+                        new Location(world, 249, 180, 74),
+                        Sound.GENESIS_INCEPTION
+                )
+        );
+
+        registerListener(
+                new Room(
+                        "falling tube",
+                        new Location(world, 222, 253, 34),
+                        new Location(world, 224, 120, 36),
+                        Sound.GENESIS_EMERGENCE
+                )
+        );
+
+        registerListener(
+                new Room(
+                        "walk toward life",
+                        new Location(world, 99, 227, 86),
+                        new Location(world, 2, 130, -71),
+                        Sound.GENESIS_BIRTH
+                )
+        );
+
+        registerListener(
+                new Room(
+                        "life spawn room",
+                        new Location(world, 259, 76, 221),
+                        new Location(world, 233, 63, 198),
+                        Sound.LIFE_INFANCY
+                )
+        );
+
+        registerListener(
+                new Room(
+                        "dark inverted pyramic",
+                        new Location(world, 309, 169, -14),
+                        new Location(world, 399, 116, 75),
+                        Sound.LIFE_INFANCY2
+                )
+        );
+
+        registerListener(
+                new Room(
+                        "think twice",
+                        new Location(world, 170, 88, 178),
+                        new Location(world, 168, 90, 180),
+                        Sound.DEATH_SHORT
+                )
+        );
+
+        registerListener(
+                new Room(
+                        "bridge",
+                        new Location(world, 208, 72, 190),
+                        new Location(world, 227, 82, 158),
+                        Sound.LIFE_ADULTHOOD
+                )
+        );
+
+        registerListener(
+                new Room(
+                        "the pit",
+                        new Location(world, 227, 81, 157),
+                        new Location(world, 223, 2, 1154),
+                        Sound.LIFE_ELDER
+                )
+        );
+
+        registerListener(
+                new Room(
+                        "regeneration",
+                        new Location(world, 173, 47, 123),
+                        new Location(world, 83, 58, 32),
+                        Sound.REGENERATION
+                )
+        );
 
         registerListener(
                 new Room(
@@ -99,6 +201,7 @@ public class Samsara extends Module<SamsaraStorageHandler> {
      * register all listeners
      */
     private void registerListeners(World world) {
+        registerListener(new DamageListener());
         registerRooms(world);
     }
 
